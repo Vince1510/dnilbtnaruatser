e99822a8519f53b01990728584457141d3d04cbe
Liam Plokkaar <l.plokkaar11@gmail.com>
2026-04-08 12:19:33 +0200

    Contact en bedankt pagina zijn klaar

---
+++ public/js/contact.js
@@ -0,0 +1,14 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  fetch(this.action, {
+    method: "POST",
+    body: new FormData(this),
+  });
+
+  this.reset();
+});
+
+function redirect() {
+  window.location.href = "Bedankt.html";
+}
